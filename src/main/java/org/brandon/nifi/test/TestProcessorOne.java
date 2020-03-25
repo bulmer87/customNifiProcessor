ad6e2e80617a0dec10915e5aa9d9e06387a13551
Brandon Ulmer <brandonulmer87@yahoo.com>
2020-03-24 18:16:06 -0600

    Did a little refactoring of the build script and the nars seem to be working. Can add a custom processor to the flow

--- src/main/java/org/brandon/nifi/test/TestProcessorOne.java
+++ src/main/java/org/brandon/nifi/test/TestProcessorOne.java
@@ -1,22 +1,98 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.brandon.nifi.test;
+
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.annotation.behavior.ReadsAttribute;
+import org.apache.nifi.annotation.behavior.ReadsAttributes;
+import org.apache.nifi.annotation.lifecycle.OnScheduled;
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.SeeAlso;
+import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.processor.exception.ProcessException;
 import org.apache.nifi.processor.AbstractProcessor;
 import org.apache.nifi.processor.ProcessContext;
 import org.apache.nifi.processor.ProcessSession;
 import org.apache.nifi.processor.ProcessorInitializationContext;
-import org.apache.nifi.processor.exception.ProcessException;
+import org.apache.nifi.processor.Relationship;
 
-public class TestProcessorOne extends AbstractProcessor
-{
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Tags({"custom"})
+@CapabilityDescription("This is a simple default processor that will use jaxb to modify the contents of a flow file")
+@SeeAlso({})
+@ReadsAttributes({@ReadsAttribute(attribute="fullyQualifiedClassName", description="The full qualified class name sent in from the" +
+        "httpPost external program")})
+public class TestProcessorOne extends AbstractProcessor {
+
+    public static final PropertyDescriptor FULL_CLASS_NAME = new PropertyDescriptor
+            .Builder().name("FULL_CLASS_NAME")
+            .displayName("Fully qualified class name")
+            .description("This will override the class name sent in by the http post program (not recommended)")
+            .required(false)
+            .build();
+
+    public static final Relationship SUCCESS = new Relationship.Builder()
+            .name("SUCCESS")
+            .description("If a class is successfully parsed and converted into an IPC message the flow file with the IPC" +
+                    " payload will be sent to this relationship for IPC serverconn sends")
+            .build();
+
+    private List<PropertyDescriptor> descriptors;
+    private Set<Relationship> relationships;
 
     @Override
-    public void onTrigger(ProcessContext context, ProcessSession session) throws ProcessException
-    {
+    protected void init(final ProcessorInitializationContext context) {
+        //make the property descriptors an immutable list
+        final List<PropertyDescriptor> descriptors = new ArrayList<PropertyDescriptor>();
+        descriptors.add(FULL_CLASS_NAME);
+        this.descriptors = Collections.unmodifiableList(descriptors);
+        //make the relationships an immutable list
+        final Set<Relationship> relationships = new HashSet<Relationship>();
+        relationships.add(SUCCESS);
+        this.relationships = Collections.unmodifiableSet(relationships);
+    }
+
+    @Override
+    public Set<Relationship> getRelationships() {
+        return this.relationships;
+    }
+
+    @Override
+    public final List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+        return descriptors;
+    }
+
+    @OnScheduled
+    public void onScheduled(final ProcessContext context) {
 
     }
 
     @Override
-    protected void init(ProcessorInitializationContext context)
-    {
-
+    public void onTrigger(final ProcessContext context, @org.jetbrains.annotations.NotNull final ProcessSession session) throws ProcessException {
+        FlowFile flowFile = session.get();
+        if ( flowFile == null ) {
+            return;
+        }
+        // TODO implement
     }
 }
