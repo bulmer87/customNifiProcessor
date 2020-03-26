76575cf9bf9ee62148e741fd38138e568782bae5
Brandon Ulmer <brandonulmer87@yahoo.com>
2020-03-25 21:17:52 -0600

    Seems to be in a solid state but need to do some provenance event handling and might not need to create a new flow file to keep the data provenance easy to follow just write the contents back in.

--- src/main/java/org/brandon/nifi/test/TestProcessorOne.java
+++ src/main/java/org/brandon/nifi/test/TestProcessorOne.java
@@ -48,29 +48,48 @@ import org.apache.nifi.processor.ProcessSession;
 import org.apache.nifi.processor.ProcessorInitializationContext;
 import org.apache.nifi.processor.Relationship;
 import org.apache.nifi.processor.io.InputStreamCallback;
+import org.apache.nifi.processor.util.StandardValidators;
+import org.intellij.lang.annotations.Flow;
 
 import javax.xml.bind.*;
 import javax.xml.transform.stream.StreamSource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
 import java.nio.ByteBuffer;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
 
-@Tags({"custom"})
+@Tags({"custom","processor"})
 @CapabilityDescription("This is a simple default processor that will use jaxb to modify the contents of a flow file")
 @SeeAlso({})
 @ReadsAttributes({@ReadsAttribute(attribute="fullyQualifiedClassName", description="The full qualified class name sent in from the" +
         "httpPost external program")})
 public class TestProcessorOne extends AbstractProcessor {
+
     private static final String myClassName = "classes.AttributePojo";
     //JAXB context
     private static Unmarshaller myUnmarshaller;
-
     //logger from the inherited classes
     ComponentLog myLogger;
+
+    //Property descriptors
+    //I think classpath dependencies should not be up to the user of this processor but should be done without any knowledge
+    //from those that want to implement this processor but I currently know of no other way to add classpath values to nifi
+    //other than dumping jaxb and any other apis into the libs dir of nifi and even then there is no guarantee that it will
+    //scan there. Also I dont think that for every processor that we should package jaxb and any other api over and over into
+    //that processors nar file. I need to find a way to put external libs on the nifi class path once and make that
+    //transparent to the processor user.
+    //@TODO find a way to add external classpath to nifi without nar file delivery or making the user input that path
+    PropertyDescriptor EXTERNAL_RESOURCE_DIRECTORY = new PropertyDescriptor.Builder()
+            .name("Extra Resource Directory")
+            .description("This is the external dependencies to directory to make this processor execute in execution")
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .expressionLanguageSupported(true)
+            .dynamicallyModifiesClasspath(true)
+            .build();
 
     //Relationships
     public static final Relationship SUCCESS = new Relationship.Builder()
@@ -93,7 +112,12 @@ public class TestProcessorOne extends AbstractProcessor {
         //get the logger
         myLogger = getLogger();
 
-        //make the relationships an immutable list
+        //make the properties an immutable list
+        final List<PropertyDescriptor> descriptors = new ArrayList<>();
+        descriptors.add(EXTERNAL_RESOURCE_DIRECTORY);
+        this.descriptors = Collections.unmodifiableList(descriptors);
+
+        //make the relationships an immutable set
         final Set<Relationship> relationships = new HashSet<Relationship>();
         relationships.add(SUCCESS);
         relationships.add(FAILURE);
@@ -118,16 +142,19 @@ public class TestProcessorOne extends AbstractProcessor {
     }
 
     @OnScheduled
-    public void onScheduled(final ProcessContext context) {
+    public void onScheduled(final ProcessContext context)
+    {
 
     }
 
     @Override
-    public void onTrigger(final ProcessContext context, final ProcessSession session) throws ProcessException {
+    public void onTrigger(final ProcessContext context, final ProcessSession session) throws ProcessException
+    {
         FlowFile flowFile = session.get();
         //great not a null flow file expected
         if(flowFile != null)
         {
+            //read the contents of the flow file
             final String xmlContents = readContents(flowFile, session);
             StreamSource streamSource = new StreamSource(new StringReader(xmlContents));
             AttributePojo attributePojo = null;
@@ -138,12 +165,33 @@ public class TestProcessorOne extends AbstractProcessor {
             }
             catch(JAXBException unmarshallE)
             {
+                sendToFailure(flowFile,session);
                 throw new ProcessException("TestProcessorOne has thrown a unmarshal exception " + unmarshallE.toString());
             }
             //now process the AttributePojo
             if(attributePojo != null)
             {
-                byte[] apBytes = attributePojo.fillBuffer();
+                byte[] apBytes = new byte[0];
+                try
+                {
+                    apBytes = attributePojo.fillBuffer();
+                }
+                catch (UnsupportedEncodingException e)
+                {
+                    sendToFailure(flowFile,session);
+                    throw new ProcessException("TestProcessorOne has thrown a Unsupported Encoding Exception " + e.toString());
+                }
+                //now that we have the correct payload we need to send this to a socket processor to send this out
+                //but first a new flow file to store the payload and then write the byte array to the flow file
+                FlowFile newff = session.get();
+                writeContents(apBytes,newff,session);
+                sendToSuccess(newff,session);
+            }
+            else
+            {
+                sendToFailure(flowFile,session);
+                throw new ProcessException("The unmarshalled pojo object is null after processing the flow file xml" +
+                        " with id: " + flowFile.getId());
             }
 
         }
@@ -153,31 +201,84 @@ public class TestProcessorOne extends AbstractProcessor {
         }
     }
 
+    /**
+     *
+     * @param T
+     * @return Unmarshaller
+     * @throws JAXBException
+     * This is a utility function that will get the jaxb unmarshaller for the class that it is processing
+     * from httpPost program
+     * Should be part of the base class
+     */
     private Unmarshaller getJAXBUnMarshaller(Class T) throws JAXBException
     {
         JAXBContext context = JAXBContext.newInstance(T);
         return context.createUnmarshaller();
     }
 
+    /**
+     * @param ff FlowFile
+     * @param session ProcessSession
+     * @return String XML contents of the flow file
+     * A quick and dirty way to read the flow contents into a string.
+     * All flow file contents coming into test processors will be a marshalled string
+     * of the class contents from an xsd schema
+     * I want this to be part of the base class
+     */
     private String readContents(final FlowFile ff, final ProcessSession session)
     {
         final AtomicReference<String> contentsRef = new AtomicReference<>(null);
-        session.read(ff, new InputStreamCallback()
+        session.read(ff, in ->
         {
-            @Override
-            public void process(InputStream in) throws IOException
-            {
-                final String contents = IOUtils.toString(in,"UTF-8");
-                contentsRef.set(contents);
-            }
+            final String contents = IOUtils.toString(in,"UTF-8");
+            contentsRef.set(contents);
         });
         return contentsRef.get();
+    }
+
+    /**
+     *
+     * @param writeArray
+     * @param ff
+     * @param session
+     * Quick function to write the Pojo byte contents to a flow file
+     */
+    private void writeContents(byte[] writeArray, FlowFile ff ,final ProcessSession session)
+    {
+        session.write(ff,out ->
+        {
+            out.write(writeArray);
+            out.flush();
+        });
+    }
+
+    /**
+     *
+     * @param ff
+     * @param session
+     * A helper to send the flow file to the failure relationship
+     */
+    private void sendToFailure(FlowFile ff, ProcessSession session)
+    {
+        session.transfer(ff,FAILURE);
+    }
+
+    /**
+     *
+     * @param ff
+     * @param session
+     * A helper to send the newly created flow file to the success relationship
+     */
+    private void sendToSuccess(FlowFile ff, ProcessSession session)
+    {
+        session.transfer(ff,SUCCESS);
     }
 
     /**
      * inner factory to manage contexts created by any
      * threads that execute this processor. singleton with
      * a static reference at the processor level
+     * Thread safe
      */
     class JaxbFactory
     {
